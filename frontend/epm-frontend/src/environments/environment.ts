585b7e7c3a038a504c3a6bb6855d95dc500dad95
Erick <erickjo10x@gmail.com>
2026-06-04 18:35:48 +0200

    feat: update notification models (NotificationPreference, NotificationChannel)

--- frontend/epm-frontend/src/environments/environment.ts
+++ frontend/epm-frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api/v1',
+  wsBaseUrl: 'ws://localhost:8080/ws/notifications',
   keycloak: {
     issuer: 'http://localhost:8180/realms/epm',
     clientId: 'epm-frontend',
